d7f52923310533f93c2b76b983545007251baa41
Cédric Vindevogel <r0944241@student.vives.be>
2023-04-03 00:19:37 +0200

    Test met send data.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO.Ports;
+using System.Drawing;
 
 namespace Cédric_Vindevogel___Project_OOP
 {
@@ -20,16 +22,59 @@ namespace Cédric_Vindevogel___Project_OOP
     /// </summary>
     public partial class MainWindow : Window
     {
+        SerialPort _serialPort;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            _serialPort = new SerialPort("COM3");
+            _serialPort.Open();
+
+            //cbxComPorts.Items.Add("None");
+            //foreach (string port in SerialPort.GetPortNames())
+            //    cbxComPorts.Items.Add(port);
         }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if ((_serialPort != null) && (_serialPort.IsOpen))
+            {
+                _serialPort.Write(new byte[] { 0 }, 0, 1);
+
+                // Gooi alle info van de _serialPort in de vuilbak.
+                _serialPort.Dispose();
+            }
+        }
+
+        //private void cbxComPorts_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        //{
+        //    if (_serialPort != null)
+        //    {
+        //        if (_serialPort.IsOpen)
+        //            _serialPort.Close();
+
+        //        if (cbxComPorts.SelectedItem.ToString() != "None")
+        //        {
+        //            _serialPort.PortName = cbxComPorts.SelectedItem.ToString();
+        //            _serialPort.Open();
+        //        }
+        //    }
+        //}
 
         private void Add_Click(object sender, RoutedEventArgs e)
         {
             CheckBox item = new CheckBox();
             item.Content = tbxToevoegen.Text;
             lbxLichtkrant.Items.Add(item);
+            SendData();
+        }
+
+        private void SendData()
+        {
+            string pagina = "A";
+
+            _serialPort.Write("<ID01><RP" + pagina + ">" + Convert.ToChar(13) + Convert.ToChar(10));
         }
     }
 }
